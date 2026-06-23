04f28437dbef29097dfa090aaf087559435012a6
dogaha <ekhangv@gmail.com>
2026-06-23 02:58:31 -0500

    finished ingestion: dag and loading.py

--- es-pipeline/airflow/dags/es-pipeline_ingestion.py
+++ es-pipeline/airflow/dags/es-pipeline_ingestion.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.sensors.filesystem import FileSensor
 from airflow.operators.bash import BashOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from datetime import datetime
 
 with DAG(
@@ -15,10 +16,17 @@ with DAG(
         fs_conn_id="fs_default",
         filepath="/opt/airflow/data/landing/*.txt",
         poke_interval=30,
-        timeout=60*60
+        timeout=60*5
     )
     run_load = BashOperator(
         task_id="run_load",
         bash_command="python /opt/airflow/load.py"
     )
-    wait_for_file >> run_load
+
+    trigger_self = TriggerDagRunOperator(
+        task_id="trigger_self",
+        trigger_dag_id="es_pipeline_ingest",  # same DAG id
+        wait_for_completion=False
+    )
+    
+    wait_for_file >> run_load>> trigger_self
--- es-pipeline/scripts/load.py
+++ es-pipeline/scripts/load.py
@@ -1,13 +1,13 @@
-# Silver es-data
-# id            BIGSERIAL PRIMARY KEY,
-# bar_timestamp TEXT NOT NULL,   -- raw string, e.g. '20241213 060100'
-# open_price    NUMERIC,
-# high_price    NUMERIC,
-# low_price     NUMERIC,
-# close_price   NUMERIC,
-# volume        NUMERIC,
-# source_file   TEXT NOT NULL,
-# loaded_at     TIMESTAMP NOT NULL DEFAULT now()
+# bronze.es_bars
+#   id            BIGSERIAL PRIMARY KEY,
+#   bar_timestamp TEXT NOT NULL,   -- raw string, e.g. '20241213 060100'
+#   open_price    NUMERIC,
+#   high_price    NUMERIC,
+#   low_price     NUMERIC,
+#   close_price   NUMERIC,
+#   volume        NUMERIC,
+#   source_file   TEXT NOT NULL,
+#   loaded_at     TIMESTAMP NOT NULL DEFAULT now()
 
 
 try:
@@ -15,14 +15,21 @@ try:
     import os
     import logging
     import psycopg2
+    import io
     from dotenv import load_dotenv
 
     load_dotenv()
-    logging.basicConfig(
-        filename="/opt/airflow/logs/pipeline.log",
-        level=logging.INFO,
-        format="%(asctime)s %(levelname)s %(message)s"
-    )
+    logger = logging.getLogger("es_pipeline")
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler("/opt/airflow/logs/pipeline.log")
+    handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
+    logger.addHandler(handler)
+
+    # logging.basicConfig(
+    #     filename="/opt/airflow/logs/pipeline.log",
+    #     level=logging.INFO,
+    #     format="%(asctime)s %(levelname)s %(message)s"
+    # )
 
     # Ingestion
     conn = psycopg2.connect(
@@ -32,13 +39,34 @@ try:
         user=os.getenv("POSTGRES_USER"),
         password=os.getenv("POSTGRES_PASSWORD")
     )
-    
-
     cur = conn.cursor()
+
+    landingPath = "opt/airflow/data/landing"
+    archivePath = "opt/airflow/data/archive"
+
+    for instance in os.listdir(landingPath):
+        if instance.endswith(".txt"):
+            instancePath = os.path.join(landingPath,instance)
+            if os.path.isfile(instancePath):
+                try:
+                    filename=os.path.basename(instancePath)
+                    buffer = io.StringIO()
+                    with open(instancePath,"r",encoding="utf-8") as file:
+                        for line in file:
+                            buffer.write(line.strip()+f";{filename}\n")
+                    buffer.seek(0)
+
+                    cur.copy_expert(
+                        "COPY bronze.es_bars (bar_timestamp, open_price, high_price, low_price, close_price, volume, source_file) FROM STDIN WITH(FORMAT csv, DELIMITER ';')",
+                        buffer
+                    )
+
+                    logger.info(f"Successfully Ingested {filename}")
+                except Exception as e:
+                    logger.exception(f"Parse Error: {filename}")
     conn.commit()
-    logging.info("Successful Ingestion")
 except Exception as e:
-    logging.exception("Unexpected Error")
+    logger.exception("Unexpected Error")
 finally:
     cur.close()
     conn.close()
